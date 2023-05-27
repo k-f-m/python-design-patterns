dcd16a51ae3784b469742a70116ef2e504969b10
k-f-m <55965735+k-f-m@users.noreply.github.com>
2023-05-27 23:17:44 +0200

    Add Singleton pattern example in Python

---
+++ abstract-factory/singleton.py
@@ -0,0 +1,24 @@
+"""
+This code shows how the Singleton pattern can be implemented in Python.
+The Singleton pattern ensures that a class has only one instance and provides a global point of access to it.
+"""
+
+class Singleton:
+    __instance = None
+
+    @staticmethod 
+    def getInstance():
+        if Singleton.__instance == None:
+            Singleton()
+        return Singleton.__instance
+
+    def __init__(self):
+        if Singleton.__instance != None:
+            raise Exception("This class is a singleton!")
+        else:
+            Singleton.__instance = self
+
+    def showMessage(self):
+        print("An instance of a Singleton")
+
+Singleton.getInstance().showMessage()
